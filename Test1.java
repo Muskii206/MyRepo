7abf31e20a99e2fe940ee61946d474ad05ddb595
Muskii206 <muskanofficial72@gmail.com>
2026-05-15 11:22:31 +0530

    Add files via upload

---
+++ Test1.java
@@ -0,0 +1,24 @@
+package day_13_Collection;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+//		 wrapper class = Integer , Double
+//       autoboxing / boxing = it convert primitive data type into object
+		  int a = 10;
+		  System.out.println("a : "+a);
+		  
+		  Integer i = a;
+		  System.out.println("i :"+i);
+		  
+		//unboxing = convert object from into the primitive data type 
+		  
+		  Double d = 300000.0;
+		  double newD = d;
+		  
+		  
+		// TODO Auto-generated method stub
+
+	}
+
+}
