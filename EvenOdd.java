ad106244c32ce2d27b6e312bf6a4fa163bf17cd4
Muskii206 <muskanofficial72@gmail.com>
2026-05-17 15:32:11 +0530

    Add files via upload

---
+++ EvenOdd.java
@@ -0,0 +1,20 @@
+package Assignment;
+
+public class EvenOdd {
+
+	public static void main(String[] args) {
+		int num = 15;
+	
+		if (num %2 ==0)
+			
+		System.out.println("even");
+
+		else {
+		System.out.println("odd");
+		
+	}
+	
+
+	}
+
+}
